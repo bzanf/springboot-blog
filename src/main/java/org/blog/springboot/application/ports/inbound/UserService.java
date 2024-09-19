976f4466b96bc4e057c015381a47ad1cbb97b09f
bzanf <borysfreitas@gmail.com>
2024-09-19 18:51:20 -0300

    Create UserService interface

---
+++ src/main/java/org/blog/springboot/application/ports/inbound/UserService.java
@@ -0,0 +1,22 @@
+package org.blog.springboot.application.ports.inbound;
+
+import org.blog.springboot.domain.entities.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+    User findById(String id);
+    List<User> findAll();
+
+    void deleteById();
+    void deleteAll();
+
+    User insert(User user);
+    List<User> insert(List<User> users);
+
+    User update(User user);
+    List<User> update(List<User> users);
+
+}
