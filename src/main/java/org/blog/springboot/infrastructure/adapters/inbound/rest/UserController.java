d477381806999f0f0dafef46ccfb043d4b588f87
bzanf <borysfreitas@gmail.com>
2024-09-19 18:57:47 -0300

    Add findById and insert mapping for users

--- src/main/java/org/blog/springboot/infrastructure/adapters/inbound/rest/UserController.java
+++ src/main/java/org/blog/springboot/infrastructure/adapters/inbound/rest/UserController.java
@@ -1,13 +1,13 @@
 package org.blog.springboot.infrastructure.adapters.inbound.rest;
 
-import org.blog.springboot.application.services.UserService;
+import org.blog.springboot.application.ports.inbound.UserService;
 import org.blog.springboot.domain.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,7 +18,20 @@ public class UserController {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAlList() {
+    public ResponseEntity<List<User>> findAll() {
         return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        User newUser = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId()).toUri();
+        return ResponseEntity.created(uri).body(newUser);
     }
 }
